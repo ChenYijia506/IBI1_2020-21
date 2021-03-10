7aea265e9ae5fd4cd7a9c7ce68e9abdb89510913
unknown <yijia.20@intl.zju.edu.cn>
2021-03-10 14:22:01 +0800

    edit fibonacci

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -11,13 +11,13 @@
 
 #Define the first and second terms of fibonacci sequence.
 X = 1
-print("第1项是"， X）
+print("No.1 term is"， X）
 Y = 1
-print("第2项是", Y)
+print("No.2 term is", Y)
 #Create a loop to calculate the other terms of the sequence.
 for i in range(3,14):
 Z = X + Y
 X = Y
 Y = Z
 #Then print the terms of the sequence.
-print("第", i, "项是", Z)
+print("No.", i, "term is", Z)
