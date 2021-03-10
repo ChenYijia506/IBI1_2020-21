e9b94b5861a3d2b0c2786c92d4eeeb5fe4515319
unknown <yijia.20@intl.zju.edu.cn>
2021-03-10 10:32:25 +0800

    Initialise

---
+++ Practical5/r_rate.py
@@ -0,0 +1,15 @@
+#define r = 1.2 n = 84
+#for loops in range(1,6):
+#Multiply
+#multiplying()
+#How many loops completed?
+#If less than 5: keep multiplying
+#If 5: DONE!
+#if(loops == 5):
+#done()
+
+r = 1.2
+n = 84
+for i in range(1,6):
+n = n*r
+print(n)
